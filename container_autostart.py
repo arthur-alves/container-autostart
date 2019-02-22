93ee3fe0f5b39de3329a60278dabe7a4be6284e0
Arthur Alves <arthur.4lves@gmail.com>
2019-02-22 16:53:51 -0300

    Update container_autostart.py

--- container_autostart.py
+++ container_autostart.py
@@ -1,43 +1,44 @@
 # -*- coding: utf-8 -*-
-u"""Este pequeno módulo serve para iniciar o docker em caso de parada.
+u"""This small module is usefull to monitor docker process and if it stops, we
+can automatically restart it.
 
-Para isto, basta enviar id do container ativo para monitorar, e cadastrar
-este script no cron.
+For this, we should send id of the active container to monitoring, and set 
+the script on the crontab.
 
-OBS: os prints abaixo servem para logar o que está acontecendo no log
-do crontab
+PS: The print statement is only to log what is happening, we probably will 
+fix it to the future log tool.
 """
 from commands import getstatusoutput
 
 
 class ContainerAutoStart(object):
-    u"""Reinicia docker automaticamente em caso de parada."""
+    u"""Auto restart container if stop cases."""
 
     def __init__(self, container_id):
-        u"""container_id deve ser uma string contendo o ID do container."""
+        u"""container_id should be a string with the ID."""
         self.container_id = container_id
 
     def supervisor(self):
-        u"""Processo responsável por verificar se docker está rodando."""
+        u"""Check if docker is running."""
         ps = getstatusoutput("docker ps")
         if 0 in ps:
             if self.container_id not in ps[1]:
-                print "Reiniciando Docker"
+                print "Restarting Docker"
                 self.start_container()
             else:
-                print "Docker rodando normalmente"
+                print "Docker is running now"
 
     def start_container(self):
-        u"""Processo deve ser invocado para iniciar o container."""
+        u"""Start the container based on container id provided."""
         ct_id = self.container_id
         ps = getstatusoutput("docker start {}".format(ct_id))
         if ct_id not in ps:
             self.supervisor()
         else:
-            print "Docker iniciado com sucesso"
+            print "Docker started"
 
 
-# Exemplo de uso
+# How to use it.
 if __name__ == "__main__":
     dk = ContainerAutoStart("97ebaea37f47")
     dk.supervisor()
